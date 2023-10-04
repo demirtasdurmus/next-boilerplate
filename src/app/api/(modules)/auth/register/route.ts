54fb6d6c7e0375c4eaf2258f6930411b6e93c837
Durmuş <durmus.demirtas@anywai.ch>
2023-10-04 13:38:35 +0300

    docs: add a comment note about inserting values in jsonb arrays is postgres

--- src/app/api/(modules)/auth/register/route.ts
+++ src/app/api/(modules)/auth/register/route.ts
@@ -9,11 +9,11 @@ import {
 } from '@/app/api/_utils/build-success-response.util';
 import { parseRequestBodyMiddleware } from '@/app/api/_middlewares/parse-request-body.middleware';
 import { db } from '@/app/api/_db';
-import { eq, sql } from 'drizzle-orm';
+// import { eq, sql } from 'drizzle-orm';
 import { TRegisterDto, registerDto } from '../_dto/register.dto';
-// import { hashPassword } from '../_utils/hash-password.util';
+import { hashPassword } from '../_utils/hash-password.util';
 import * as schemas from '../../../_db/schema';
-// import { getUsernameFromEmail } from '../_utils/get-username-from-email.util';
+import { getUsernameFromEmail } from '../_utils/get-username-from-email.util';
 
 export type TRegisterResponse = TSuccessResponse<
   unknown,
@@ -24,23 +24,25 @@ export type TRegisterResponse = TSuccessResponse<
 
 async function registerHandler(
   _req: NextRequest,
-  _ctx: IPublicRequestContext<TRegisterDto>,
+  ctx: IPublicRequestContext<TRegisterDto>,
 ): Promise<NextResponse<TRegisterResponse>> {
-  // const hashedPassword = await hashPassword(ctx.payload.password);
-  // const username = getUsernameFromEmail(ctx.payload.email);
+  const hashedPassword = await hashPassword(ctx.payload.password);
+  const username = getUsernameFromEmail(ctx.payload.email);
 
-  // await db.insert(schemas.users).values({
-  //   email: ctx.payload.email,
-  //   password: hashedPassword,
-  //   username,
-  // });
+  await db.insert(schemas.users).values({
+    email: ctx.payload.email,
+    password: hashedPassword,
+    username,
+  });
 
-  await db
-    .update(schemas.users)
-    .set({
-      roles: sql`roles + ${['ADMIN']}`,
-    })
-    .where(eq(schemas.users.id, '01ea2e89-a3fc-43c4-97fc-a83451110cb4'));
+  /* Here is how you can new values to a jsonb array */
+  // await db
+  //   .update(schemas.users)
+  //   .set({
+  //     roles: sql`jsonb_insert(roles, '{-1}', ${schemas.Role.ADMIN}::jsonb, true)`,
+  //     // someOtherComplexObjArray: sql`jsonb_insert(roles, '{-1}', ${JSON.stringify(complexObj)}::jsonb)`,
+  //   })
+  //   .where(eq(schemas.users.id, '01ea2e89-a3fc-43c4-97fc-a83451110cb4'));
 
   return buildCreatedResponse({
     data: {},
