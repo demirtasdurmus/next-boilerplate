19ef97504dc14dbb515f8c1c53d6fc57dccec37a
Durmuş <durmus.demirtas@anywai.ch>
2023-10-23 07:46:43 +0300

    docs: add a mock suspense example to show how server streaming works

--- src/app/(ui)/products/page.tsx
+++ src/app/(ui)/products/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 import SearchPage from './search-page';
 
 export default function Products() {
@@ -6,6 +7,13 @@ export default function Products() {
     <div className="mx-24 mt-12 flex flex-col gap-8">
       <h1 className="text-5xl">Products</h1>
       <SearchPage />
+      {/* Suspense example, Divides code into chunks,
+       so react doesn't wait for the whole page to hydrate,
+       the server streams the pre-rendered content and react hydrates them in chunks */}
+      <Suspense fallback={<p>Loading Product list</p>}>
+        {/* <ProductList /> */}
+        <p>This is a server component that fetches data before prerender</p>
+      </Suspense>
       <Link
         href="/products/unique-id?showDialog=y"
         className="text-3xl underline"
