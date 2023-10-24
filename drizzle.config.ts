7851d2a665e67d95a1b382be1061996c7b767119
Durmuş <durmus.demirtas@anywai.ch>
2023-10-24 17:11:20 +0300

    fix: drizzle config schema and out dir paths

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
+import type { Config } from 'drizzle-kit';
 import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '.env.local') });
@@ -9,8 +9,8 @@ if (!process.env.DATABASE_URL) {
 }
 
 export default {
-  schema: './src/app/api/_db/schema.ts',
-  out: './src/app/api/_db/migrations',
+  schema: './src/app/(server)/_db/schema.ts',
+  out: './src/app/(server)/_db/migrations',
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
