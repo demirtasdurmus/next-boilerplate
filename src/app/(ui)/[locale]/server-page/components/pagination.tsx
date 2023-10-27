e0ddd90294770bf05ac444e25ebac07fbe2ff4a4
Durmuş <durmus.demirtas@anywai.ch>
2023-10-27 22:51:42 +0300

    feat: add search bar to server component

--- src/app/(ui)/[locale]/server-page/components/pagination.tsx
+++ src/app/(ui)/[locale]/server-page/components/pagination.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { defaultGetExamplesLimit } from '@/app/(server)/api/examples/_schemas/get-examples.dto';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
@@ -13,16 +12,14 @@ type Props = {
 export default function Pagination({ totalPages, hasMore }: Props) {
   const searchParams = useSearchParams();
   const pageParam = Number(searchParams.get('page')) || 1;
-  const limitParam =
-    Number(searchParams.get('limit')) || defaultGetExamplesLimit;
   const q = searchParams.get('q') || '';
 
   const router = useRouter();
   const [page, setPage] = useState(pageParam);
 
   useEffect(() => {
-    router.push(`/server-page?page=${page}&limit=${limitParam}&q=${q}`);
-  }, [page, q, router, pageParam, limitParam]);
+    router.push(`/server-page?page=${page}&q=${q}`);
+  }, [page, q, router]);
 
   return (
     <div className="flex space-x-2">
@@ -35,7 +32,7 @@ export default function Pagination({ totalPages, hasMore }: Props) {
         Prev
       </button>
       <span className="rounded-full bg-gray-400 p-2">
-        {pageParam}/{totalPages}
+        {pageParam <= totalPages ? pageParam : totalPages}/{totalPages}
       </span>
       <button
         type="button"
--- src/app/(ui)/[locale]/server-page/page.tsx
+++ src/app/(ui)/[locale]/server-page/page.tsx
@@ -5,23 +5,27 @@ import { fetchExamples } from '@/services/example.service';
 import { notFound } from 'next/navigation';
 import ExampleCard from './components/example-card';
 import Pagination from './components/pagination';
+import SearchBar from './components/search-bar';
 
 type Props = {
   searchParams: {
     page: number | undefined;
-    limit: number | undefined;
     q: string | undefined;
   };
 };
 
 export default async function Products({ searchParams }: Props) {
   const st = await getScopedI18n('ServerPage');
-  const { page = 1, limit = defaultGetExamplesLimit, q = '' } = searchParams;
+  const { page = 1, q = '' } = searchParams;
 
   let examples: TGetExamplesResponse['data'] = [];
   let meta: TGetExamplesResponse['meta'];
   try {
-    const res = await fetchExamples({ page, limit, q });
+    const res = await fetchExamples({
+      page,
+      limit: defaultGetExamplesLimit,
+      q,
+    });
     examples = res.data;
     meta = res.meta;
   } catch (error: any) {
@@ -33,6 +37,9 @@ export default async function Products({ searchParams }: Props) {
     <div className="mx-24 my-12 flex flex-col items-center gap-8">
       {/* Title */}
       <h1 className="text-3xl">{st('title')}</h1>
+
+      {/* Search Bar */}
+      <SearchBar />
 
       {/* Examples */}
       <div className="flex w-full flex-wrap justify-center gap-8">
